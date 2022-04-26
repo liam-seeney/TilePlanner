b41b895c8f5733b08c5d6de1603f43c69cd2dac2
Liam <liam.seeney@outlook.com>
2022-04-26 20:45:22 +0100

    Add models
    Adds a class library that holds Wall and
    Tile models.

---
+++ TPDataLibrary/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPDataLibrary
+{
+    public class Tile
+    {
+        private int _tileHeight;
+        private int _tileWidth;
+        public int TileHeight
+        {
+            get { return _tileHeight; }
+            set { _tileHeight = value; }
+        }
+        public int TileWidth 
+        {
+            get { return _tileWidth; }
+            set { _tileWidth = value; }
+        } 
+    }
+}
---
+++ TPDataLibrary/Wall.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPDataLibrary
+{
+    public class Wall
+    {
+        private int _wallHeight;
+        private int _wallWidth;
+
+        public int WallHeight
+        {
+            get { return _wallHeight; }
+            set { _wallHeight = value; }
+        }
+        public int WallWidth 
+        {
+            get { return _wallWidth; } 
+            set { _wallWidth = value; } 
+        }  
+    }
+}
