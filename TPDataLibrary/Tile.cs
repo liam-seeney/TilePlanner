219bc0165432277151bbeed2ed06585dfa60f877
Liam <lms.seeney@gmail.com>
2022-04-26 21:34:04 +0100

    Update Wall and Tile class
    Update classes

--- TPDataLibrary/Tile.cs
+++ TPDataLibrary/Tile.cs
@@ -10,15 +10,23 @@ namespace TPDataLibrary
     {
         private int _tileHeight;
         private int _tileWidth;
+        private int _tileArea;
+
         public int TileHeight
         {
             get { return _tileHeight; }
             set { _tileHeight = value; }
         }
-        public int TileWidth 
+        public int TileWidth
         {
             get { return _tileWidth; }
             set { _tileWidth = value; }
-        } 
+        }
+
+        public int TileArea
+        {
+            get { return _tileArea; }
+            set { _tileArea = value; }
+        }
     }
 }
--- TPDataLibrary/Wall.cs
+++ TPDataLibrary/Wall.cs
@@ -10,16 +10,28 @@ namespace TPDataLibrary
     {
         private int _wallHeight;
         private int _wallWidth;
+        private int _areaOfWall;
+        private int _centerPointOfWall;
 
         public int WallHeight
         {
             get { return _wallHeight; }
             set { _wallHeight = value; }
         }
-        public int WallWidth 
+        public int WallWidth
         {
-            get { return _wallWidth; } 
-            set { _wallWidth = value; } 
-        }  
+            get { return _wallWidth; }
+            set { _wallWidth = value; }
+        }
+        public int AreaOfWall
+        {
+            get { return _areaOfWall; }
+            set { _areaOfWall = _wallHeight * _wallWidth; }
+        }
+        public int CenterPointOfWall
+        {
+            get { return _centerPointOfWall; }
+            set { _centerPointOfWall = _wallWidth / 2; }
+        }
     }
 }
